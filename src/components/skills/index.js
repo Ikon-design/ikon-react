deee7e29fe3f924b08e5b0225d6a9715eb0f1715
Ikon-design <77398364+Ikon-design@users.noreply.github.com>
2021-09-27 14:25:11 +0200

    Debut du responsive

--- src/components/skills/index.js
+++ src/components/skills/index.js
@@ -1,40 +1,101 @@
-import React from "react";
+import React, {useState} from "react";
+
+import ArrowLeft from "../../images/arrow-left.png"
+import ArrowRight from "../../images/arrow-right.png"
+import CarreBlanc from "../../images/carreBlanc.png"
+import CarreVert from "../../images/carreVert.png"
+import CarreJaune from "../../images/carreJaune.png"
+import CarreViolet from "../../images/carreViolet.png"
+import CarreRouge from "../../images/carreRouge.png"
 
 import "./skills.css"
 
 const Skills = () => {
+    const skillsData = [
+        {
+            id: "1",
+            ligne: 1,
+            image: CarreBlanc,
+            title: "UI/UX",
+            description: "Création d'interface ergonomique pour les sites, applications et jeux."
+        },
+        {
+            id: "2",
+            ligne: 1,
+            image: CarreJaune,
+            title: "développement",
+            description: "Développement d'application bureau, mobile, de site web."
+        },
+        {
+            id: "3",
+            ligne: 1,
+            image: CarreRouge,
+            title: "Graphisme",
+            description: "Création de charte graphique, illustration, logo et de documents."
+        },
+        {
+            id: "4",
+            ligne: 2,
+            image: CarreVert,
+            title: "modélisation 3D",
+            description: "Création de model 3D destiné à l'animation ou à l'impression 3D."
+        },
+        {
+            id: "5",
+            ligne: 2,
+            image: CarreViolet,
+            title: "Motion",
+            description: "Création de motion design, d'animation et de montage."
+        }
+    ]
+
+    const screenSize = document.body.offsetWidth
+    const [count, setCount] = useState(0)
+    const [currentItem, setCurrentitem] = useState(skillsData[count])
+    const changeCount = () => {
+        setCount(count + 1)
+        if (count >= skillsData.length - 1 ) setCount(0)
+        setCurrentitem(skillsData[count])
+    }
+    const downCount = () => {
+        setCount(count - 1)
+        if (count <= 0 ) setCount(skillsData.length -1)
+        setCurrentitem(skillsData[count])
+    }
     return (
         <div className={"skills-container display-flex flex-direction-column justify-content-space-evenly"}>
-            <h2 style={{ textAlign: "center"}} className={"title-text"}>Compétences</h2>
-            <div className={"display-flex justify-content-space-around"}>
-                <div className={"display-flex align-items-center flex-direction-column skills-item"}>
-                    <img className={"skills-img"}/>
-                    <h4 className={"skills-title"}>UI/UX</h4>
-                    <p className={"content-text text-align-center skills-item-description"}>Création d'interface ergonomique pour les <br/>sites, applications et jeux.</p>
-                </div>
-                <div className={"display-flex align-items-center flex-direction-column skills-item"}>
-                    <img className={"skills-img"}/>
-                    <h4 className={"skills-title"}>développement </h4>
-                    <p className={"content-text text-align-center skills-item-description"}>Développement d'application bureau, mobile,  <br/>de site web.</p>
-                </div>
-                <div className={"display-flex align-items-center flex-direction-column skills-item"}>
-                    <img className={"skills-img"}/>
-                    <h4 className={"skills-title"}>Graphisme </h4>
-                    <p className={"content-text text-align-center skills-item-description"}>Création de charte graphique, illustration,  <br/>logo et de documents.</p>
-                </div>
+            <h2 style={{textAlign: "center"}} className={"title-text"}>Compétences</h2>
+            <div className={"display-flex justify-content-space-around skills-item-container"}>
+                {screenSize < 480 ? <a style={{color: 'white'}} onClick={downCount}><img src={ArrowLeft}/></a> : null}
+                {screenSize > 480 ?
+                   <DesktopContent skillsData={skillsData}/>
+                    :
+                    <MobileContent currentItem={currentItem}/>
+                }
+                {screenSize < 480 ? <a style={{color: 'white'}} onClick={changeCount}><img src={ArrowRight}/></a> : null}
             </div>
-            <div className={"display-flex justify-content-space-evenly"}>
-                <div className={"display-flex align-items-center flex-direction-column skills-item"}>
-                    <img className={"skills-img"}/>
-                    <h4 className={"skills-title"}>modélisation 3D </h4>
-                    <p className={"content-text text-align-center skills-item-description"}>Création de model 3D destiné à l'animation <br/> ou à l'impression 3D.</p>
-                </div>
-                <div className={"display-flex align-items-center flex-direction-column skills-item"}>
-                    <img className={"skills-img"}/>
-                    <h4 className={"skills-title"}>Motion</h4>
-                    <p className={"content-text text-align-center skills-item-description"}>Création de motion design, d'animation et de  <br/> montage.</p>
-                </div>
+        </div>
+    )
+}
+
+const DesktopContent = (props) => {
+    return(
+        props.skillsData.map(x =>
+            <div className={"display-flex align-items-center justify-content-center flex-direction-column skills-item"}>
+                <img className={"skills-img"} alt={"logo de " + x.title} src={x.image}/>
+                <h4 className={"skills-title"}>{x.title}</h4>
+                <p className={"content-text text-align-center skills-item-description"}>{x.description}</p>
             </div>
+        )
+    )
+}
+
+const MobileContent = (props) => {
+    return(
+        <div className={"display-flex align-items-center flex-direction-column skills-item"}>
+            <img alt={"logo de " + props?.currentItem?.title} src={props?.currentItem?.image}/>
+            <h4 className={"skills-title"}>{props?.currentItem?.title}</h4>
+            <p className={"content-text text-align-center skills-item-description"}>{props?.currentItem?.description}</p>
         </div>
     )
 }
